a9b86c3ba1afc96c11e91b5047c1e534e9141d1f
MattiaAngelini <mattiaangelini1993@gmail.com>
2025-03-24 13:14:51 +0100

    Init Project

---
+++ script.js
@@ -0,0 +1,204 @@
+let cars = [
+      {
+        "id": 1,
+        "marca": "Fiat",
+        "modello": "Panda",
+        "anno": 2015,
+        "colore": "rosso",
+        "prezzo": 5000,
+        "carburante": "benzina",
+        "cilindrata": 1200
+      },
+      {
+        "id": 2,
+        "marca": "Ford",
+        "modello": "Focus",
+        "anno": 2018,
+        "colore": "blu",
+        "prezzo": 15000,
+        "carburante": "diesel",
+        "cilindrata": 1600
+      },
+      {
+        "id": 3,
+        "marca": "Volkswagen",
+        "modello": "Golf",
+        "anno": 2020,
+        "colore": "nero",
+        "prezzo": 20000,
+        "carburante": "benzina",
+        "cilindrata": 1400
+      },
+      {
+        "id": 4,
+        "marca": "Audi",
+        "modello": "A3",
+        "anno": 2019,
+        "colore": "grigio",
+        "prezzo": 25000,
+        "carburante": "diesel",
+        "cilindrata": 2000
+      },
+      {
+        "id": 5,
+        "marca": "BMW",
+        "modello": "Serie 1",
+        "anno": 2017,
+        "colore": "bianco",
+        "prezzo": 22000,
+        "carburante": "benzina",
+        "cilindrata": 1800
+      },
+      {
+        "id": 6,
+        "marca": "Mercedes",
+        "modello": "Classe A",
+        "anno": 2021,
+        "colore": "argento",
+        "prezzo": 30000,
+        "carburante": "diesel",
+        "cilindrata": 2200
+      },
+      {
+        "id": 7,
+        "marca": "Renault",
+        "modello": "Clio",
+        "anno": 2016,
+        "colore": "verde",
+        "prezzo": 8000,
+        "carburante": "benzina",
+        "cilindrata": 1000
+      },
+      {
+        "id": 8,
+        "marca": "Peugeot",
+        "modello": "208",
+        "anno": 2018,
+        "colore": "giallo",
+        "prezzo": 12000,
+        "carburante": "diesel",
+        "cilindrata": 1500
+      },
+      {
+        "id": 9,
+        "marca": "Toyota",
+        "modello": "Yaris",
+        "anno": 2020,
+        "colore": "rosso",
+        "prezzo": 18000,
+        "carburante": "ibrido",
+        "cilindrata": 1500
+      },
+      {
+        "id": 10,
+        "marca": "Hyundai",
+        "modello": "i20",
+        "anno": 2019,
+        "colore": "blu",
+        "prezzo": 14000,
+        "carburante": "benzina",
+        "cilindrata": 1200
+      },
+      {
+        "id": 11,
+        "marca": "Kia",
+        "modello": "Rio",
+        "anno": 2017,
+        "colore": "nero",
+        "prezzo": 11000,
+        "carburante": "diesel",
+        "cilindrata": 1400
+      },
+      {
+        "id": 12,
+        "marca": "Opel",
+        "modello": "Corsa",
+        "anno": 2018,
+        "colore": "bianco",
+        "prezzo": 13000,
+        "carburante": "benzina",
+        "cilindrata": 1000
+      },
+      {
+        "id": 13,
+        "marca": "Citroen",
+        "modello": "C3",
+        "anno": 2016,
+        "colore": "grigio",
+        "prezzo": 9000,
+        "carburante": "diesel",
+        "cilindrata": 1600
+      },
+      {
+        "id": 14,
+        "marca": "Seat",
+        "modello": "Ibiza",
+        "anno": 2020,
+        "colore": "rosso",
+        "prezzo": 16000,
+        "carburante": "benzina",
+        "cilindrata": 1000
+      },
+      {
+        "id": 15,
+        "marca": "Skoda",
+        "modello": "Fabia",
+        "anno": 2019,
+        "colore": "verde",
+        "prezzo": 15000,
+        "carburante": "diesel",
+        "cilindrata": 1200
+      },
+      {
+        "id": 16,
+        "marca": "Dacia",
+        "modello": "Sandero",
+        "anno": 2017,
+        "colore": "blu",
+        "prezzo": 7000,
+        "carburante": "benzina",
+        "cilindrata": 900
+      },
+      {
+        "id": 17,
+        "marca": "Suzuki",
+        "modello": "Swift",
+        "anno": 2018,
+        "colore": "grigio",
+        "prezzo": 12000,
+        "carburante": "benzina",
+        "cilindrata": 1300
+      },
+      {
+        "id": 18,
+        "marca": "Mazda",
+        "modello": "2",
+        "anno": 2020,
+        "colore": "rosso",
+        "prezzo": 17000,
+        "carburante": "diesel",
+        "cilindrata": 1500
+      },
+      {
+        "id": 19,
+        "marca": "Honda",
+        "modello": "Jazz",
+        "anno": 2019,
+        "colore": "nero",
+        "prezzo": 16000,
+        "carburante": "ibrido",
+        "cilindrata": 1300
+      },
+      {
+        "id": 20,
+        "marca": "Volvo",
+        "modello": "V40",
+        "anno": 2018,
+        "colore": "argento",
+        "prezzo": 22000,
+        "carburante": "diesel",
+        "cilindrata": 2000
+      }
+    ]
+    
+  console.log(cars)
